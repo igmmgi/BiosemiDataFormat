6d11cc5c48d87a2adfd5bcd68e33ed8282c5c755
igmmgi <ian.mackenzie@uni-tuebingen.de>
2018-11-12 05:10:42 +0100

    tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,16 +6,26 @@ using Test
     bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-256.bdf")
     dat = read_bdf(bdf_filename)
 
-    # some header details
     @test isequal(dat.header["num_bytes_header"], 4608)
     @test isequal(dat.header["num_channels"], 17)
     @test isequal(dat.header["num_data_records"], 60)
     @test isequal(dat.header["sample_rate"][1], 256)
-
-    # some data
     @test isequal(size(dat.data), (16, 15360))
+    @test isequal(dat.triggers["idx"][1], 415)
+    @test isequal(dat.triggers["val"][1], 255)
+    @test isequal(dat.triggers["count"][255], 40)
 
-    # triggers
+end
+
+@testset "Newtest17-256_read_bdf_channels" begin
+
+    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-256.bdf")
+    dat = read_bdf(bdf_filename, channels = [1, 3, 5])
+
+    @test isequal(dat.header["num_channels"], 5)
+    @test isequal(dat.header["num_data_records"], 60)
+    @test isequal(dat.header["sample_rate"][1], 256)
+    @test isequal(size(dat.data), (3, 15360))
     @test isequal(dat.triggers["idx"][1], 415)
     @test isequal(dat.triggers["val"][1], 255)
     @test isequal(dat.triggers["count"][255], 40)
@@ -32,27 +42,6 @@ end
     bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-256_write.bdf")
     dat2 = read_bdf(bdf_filename)
 
-    # some header details
-    @test isequal(dat1.data, dat2.data)
-    @test isequal(dat1.status, dat2.status)
-    @test isequal(dat1.time, dat2.time)
-    @test isequal(dat1.triggers, dat2.triggers)
-
-    rm(bdf_filename)
-
-end
-
-@testset "Newtest17-2048_write_bdf" begin
-
-    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048.bdf")
-    dat1 = read_bdf(bdf_filename)
-    dat1.header["filename"] = "Newtest17-2048_write.bdf"
-    write_bdf(dat1)
-
-    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048_write.bdf")
-    dat2 = read_bdf(bdf_filename)
-
-    # some header details
     @test isequal(dat1.data, dat2.data)
     @test isequal(dat1.status, dat2.status)
     @test isequal(dat1.time, dat2.time)
@@ -67,19 +56,48 @@ end
     bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048.bdf")
     dat = read_bdf(bdf_filename)
 
-    # some header details
     @test isequal(dat.header["num_bytes_header"], 4608)
     @test isequal(dat.header["num_channels"], 17)
     @test isequal(dat.header["num_data_records"], 60)
     @test isequal(dat.header["sample_rate"][1], 2048)
-
-    # some data
     @test isequal(size(dat.data), (16, 122880))
-
-    # triggers
     @test isequal(dat.triggers["idx"][1], 3353)
     @test isequal(dat.triggers["val"][1], 255)
     @test isequal(dat.triggers["count"][255], 39)
+
+end
+
+@testset "Newtest17-2048_read_bdf_channels" begin
+
+    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048.bdf")
+    dat = read_bdf(bdf_filename, channels = ["A1", "A3", "A5"])
+
+    @test isequal(dat.header["num_channels"], 4)
+    @test isequal(dat.header["num_data_records"], 60)
+    @test isequal(dat.header["sample_rate"][1], 2048)
+    @test isequal(size(dat.data), (3, 122880))
+    @test isequal(dat.triggers["idx"][1], 3353)
+    @test isequal(dat.triggers["val"][1], 255)
+    @test isequal(dat.triggers["count"][255], 39)
+
+end
+
+@testset "Newtest17-2048_write_bdf" begin
+
+    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048.bdf")
+    dat1 = read_bdf(bdf_filename)
+    dat1.header["filename"] = "Newtest17-2048_write.bdf"
+    write_bdf(dat1)
+
+    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048_write.bdf")
+    dat2 = read_bdf(bdf_filename)
+
+    @test isequal(dat1.data, dat2.data)
+    @test isequal(dat1.status, dat2.status)
+    @test isequal(dat1.time, dat2.time)
+    @test isequal(dat1.triggers, dat2.triggers)
+
+    rm(bdf_filename)
 
 end
 
@@ -88,19 +106,13 @@ end
     bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-256.bdf")
     dat1 = read_bdf(bdf_filename)
     dat2 = read_bdf(bdf_filename)
-
     dat3 = merge_bdf([dat1, dat2], "merged_datafile")
 
-    # some header details
     @test isequal(dat3.header["num_bytes_header"], 4608)
     @test isequal(dat3.header["num_channels"], 17)
     @test isequal(dat3.header["num_data_records"], 120)
     @test isequal(dat3.header["sample_rate"][1], 256)
-
-    # some data
     @test isequal(size(dat3.data), (16, 15360*2))
-
-    # triggers
     @test isequal(dat3.triggers["idx"][1], 415)
     @test isequal(dat3.triggers["val"][1], 255)
     @test isequal(dat3.triggers["count"][255], 80)
@@ -112,21 +124,49 @@ end
     bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048.bdf")
     dat1 = read_bdf(bdf_filename)
     dat2 = read_bdf(bdf_filename)
-
     dat3 = merge_bdf([dat1, dat2], "merged_datafile")
 
-    # some header details
     @test isequal(dat3.header["num_bytes_header"], 4608)
     @test isequal(dat3.header["num_channels"], 17)
     @test isequal(dat3.header["num_data_records"], 120)
     @test isequal(dat3.header["sample_rate"][1], 2048)
-
-    # some data
     @test isequal(size(dat3.data), (16, 122880*2))
-
-    # triggers
     @test isequal(dat3.triggers["idx"][1], 3353)
     @test isequal(dat3.triggers["val"][1], 255)
     @test isequal(dat3.triggers["count"][255], 78)
+
+end
+
+@testset "Newtest17-256_select_channels_bdf" begin
+
+    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-256.bdf")
+    dat = read_bdf(bdf_filename)
+
+    dat1 = select_channels_bdf(dat, channels = [1,3,5])
+
+    @test isequal(dat1.header["num_channels"], 17)
+    @test isequal(dat1.header["num_data_records"], 60)
+    @test isequal(dat1.header["sample_rate"][1], 256)
+    @test isequal(size(dat1.data), (3, 15360))
+    @test isequal(dat1.triggers["idx"][1], 415)
+    @test isequal(dat1.triggers["val"][1], 255)
+    @test isequal(dat1.triggers["count"][255], 40)
+
+end
+
+@testset "Newtest17-2048_select_channels_bdf" begin
+
+    bdf_filename = joinpath(dirname(@__FILE__), "Newtest17-2048.bdf")
+    dat = read_bdf(bdf_filename)
+
+    dat1 = select_channels_bdf(dat, channels = ["A1", "A3", "A5"])
+
+    @test isequal(dat1.header["num_channels"], 4)
+    @test isequal(dat1.header["num_data_records"], 60)
+    @test isequal(dat1.header["sample_rate"][1], 256)
+    @test isequal(size(dat1.data), (3, 15360))
+    @test isequal(dat1.triggers["idx"][1], 415)
+    @test isequal(dat1.triggers["val"][1], 255)
+    @test isequal(dat1.triggers["count"][255], 40)
 
 end
