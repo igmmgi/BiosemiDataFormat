1fb7cb6d4e30e159606ccf684b0566275f494b0c
igmmgi <ian.mackenzie@uni-tuebingen.de>
2018-11-20 02:24:22 +0100

    BioSemiData to BioSemi

--- src/BioSemiBDF.jl
+++ src/BioSemiBDF.jl
@@ -109,7 +109,7 @@ module BioSemiBDF
     "time"  => hcat(trig_val, pushfirst!(diff(trig_idx), 0) / hd["sample_rate"][1])
     )
 
-    return BioSemiData(hd, dat, time, triggers, status)
+    return BioSemi(hd, dat, time, triggers, status)
 
   end
 
